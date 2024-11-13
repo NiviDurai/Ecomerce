4273002d9bced6f65b07068328dd7b4b585f83f8
NiviDurai <join2nivi@gmail.com>
2024-11-13 12:51:09 +0530

    Update AccountController com

--- ViewBook/Controllers/AccountController.cs
+++ ViewBook/Controllers/AccountController.cs
@@ -18,7 +18,7 @@ namespace ViewBook.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
-        public AccountController()
+       // public AccountController()
         {
         }
 
